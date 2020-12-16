81dc65d52dab7f1c75c61648b67d1cbb89c9ff9b
Polyhistor <pouya.ataei.7@gmail.com>
2020-12-16 22:33:12 +1300

    debugged docker-compose

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 3ss
+          Learn Reacts
         </a>
       </header>
     </div>
